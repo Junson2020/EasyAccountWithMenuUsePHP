58b298aac59e0fd73c44212088cffcb4304cd7ed
Junson <114983215+Junson2020@users.noreply.github.com>
2023-04-27 15:12:56 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -26,16 +26,6 @@ if(empty($junsonlicense) or $junsonlicense=="") {
 }else {
 	include_once('header.php');
 	echo '<iframe id="main" name="main" height="100%" width="100%"></iframe>';
-	echo "<table border=\"1\">";
-	echo " <tr>";
-	echo "  <td><a href='accountlist.php' target='_blank'>".GetLangStr($junsonlanguage,"Account")."</a></td>";
-	echo "  <td><a href='levelmenu.php' target='_blank'>".GetLangStr($junsonlanguage,"Level")."</a></td>";
-	echo "  <td><a href='grouplist.php' target='_blank'>".GetLangStr($junsonlanguage,"Group")."</a></td>";
-	echo "  <td><a href='langlist.php' target='_blank'>".GetLangStr($junsonlanguage,"Language")."</a></td>";
-	echo "  <td><a href='textencode.php' target='_blank'>".GetLangStr($junsonlanguage,"TextEnCode")."</a></td>";
-	echo "  <td><a href='logout.php' target='_self'>".GetLangStr($junsonlanguage,"Logout")."</a></td>";
-	echo " </tr>";
-	echo "</table>";
 }
 
 ?>
